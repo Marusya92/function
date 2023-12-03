9651cc819b6151ddd3fcb3ee320dbf2638d560e8
Mariya <marusya16081992@mail.ru>
2023-12-04 00:10:04 +0300

    Выполнили третье задание

---
+++ Dritte/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая перевернёт одномерный массив
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем массив
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        // Выводим исходный массив
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        // Переворачиваем массив
+        ReverseArray(array);
+
+        // Выводим перевернутый массив
+        Console.WriteLine("Перевернутый массив:");
+        PrintArray(array);
+    }
+
+    static void ReverseArray(int[] array)
+    {
+        int length = array.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            // Обмениваем элементы симметрично относительно середины массива
+            int temp = array[i];
+            array[i] = array[length - 1 - i];
+            array[length - 1 - i] = temp;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write(number + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
